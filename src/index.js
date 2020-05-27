153b8dda32026144c0b6753b369b34b29b2e57bd
TreesZendesk <diastowo@gmail.com>
2020-05-28 00:41:53 +0700

    loop for auth

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ app.on('ready', () => {
       //# This is the default key for authentication
       const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
 
-      var blockIndexes = [8, 9, 10, 11, 12];
+      var blockIndexes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
       for (var i=0; i<blockIndexes.length; i++) {
         if (!mfrc522.authenticate(blockIndexes[i], key, uid)) {
