0fb90fd61144f43b5cadf62d7aab54a7dcebde89
Diastowo <diastowo@gmail.com>
2020-05-28 23:40:28 +0700

    stepper

--- test.js
+++ test.js
@@ -1,68 +1,67 @@
-// var Gpio = require('onoff').Gpio;
+var Gpio = require('onoff').Gpio;
 
-// var stepperEnable = new Gpio(13, 'out');
-// var stepperDir = new Gpio(19, 'out');
-// var stepperPulse = new Gpio(26, 'out');
+var stepperEnable = new Gpio(13, 'out');
+var stepperDir = new Gpio(19, 'out');
+var stepperPulse = new Gpio(26, 'out');
 
-// var blinkInterval = setInterval(blinkLED, 250);
+var blinkInterval = setInterval(blinkLED, 250);
 
-// function blinkLED() {
-//     stepperEnable.writeSync(1);
-//     stepperDir.writeSync(1);
-//     stepperPulse.writeSync(1);
-// }
+function blinkLED() {
+    stepperEnable.writeSync(1);
+    stepperDir.writeSync(1);
+    stepperPulse.writeSync(1);
+}
 
-// function endBlink() {
-// 	turnOff()
-// }
+function endBlink() {
+	turnOff()
+}
 
-// function turnOff () {
-// 	stepperPulse.writeSync(0)
-// 	stepperDir.writeSync(0)
-// 	stepperEnable.writeSync(0)
+function turnOff () {
+	stepperPulse.writeSync(0)
+	stepperDir.writeSync(0)
+	stepperEnable.writeSync(0)
 
-// 	stepperPulse.unexport()
-// 	stepperDir.unexport()
-// 	stepperEnable.unexport()
-// }
+	stepperPulse.unexport()
+	stepperDir.unexport()
+	stepperEnable.unexport()
+}
 
-// setTimeout(endBlink, 5000);
+setTimeout(endBlink, 5000);
 
-var gpio = require("gpio");
+// var gpio = require("gpio");
 
-var stepperEnable = gpio.export(13, {
-   direction: gpio.DIRECTION.OUT,
-   interval: 200,
-   ready: function() {
-   	console.log('stepperEnable ready')
-	var stepperDir = gpio.export(19, {
-	   direction: gpio.DIRECTION.OUT,
-	   interval: 200,
-	   ready: function() {
-	   	console.log('stepperDir ready')
-		var stepperPulse = gpio.export(26, {
-		   direction: gpio.DIRECTION.OUT,
-		   interval: 200,
-		   ready: function() {
-		   	console.log('stepperPulse ready')
+// var stepperEnable = gpio.export(13, {
+//    direction: gpio.DIRECTION.OUT,
+//    interval: 200,
+//    ready: function() {
+//    	console.log('stepperEnable ready')
+// 	var stepperDir = gpio.export(19, {
+// 	   direction: gpio.DIRECTION.OUT,
+// 	   interval: 200,
+// 	   ready: function() {
+// 	   	console.log('stepperDir ready')
+// 		var stepperPulse = gpio.export(26, {
+// 		   direction: gpio.DIRECTION.OUT,
+// 		   interval: 200,
+// 		   ready: function() {
+// 		   	console.log('stepperPulse ready')
 
-			stepperPulse.set(function() {
-			   console.log('stepperPulse: ' + stepperPulse.value);
-			   stepperDir.set(function() {
-			   	console.log('stepperDir: ' + stepperDir.value);
-			   	for (var i=0; i<1600; i++) {
-			   		stepperEnable.set();
-			   		stepperEnable.set(0);
-			   	}
-			   })
-			});
+// 			stepperPulse.set(function() {
+// 			   console.log('stepperPulse: ' + stepperPulse.value);
+// 			   stepperDir.set(function() {
+// 			   	console.log('stepperDir: ' + stepperDir.value);
+// 			   	for (var i=0; i<1600; i++) {
+// 			   		stepperEnable.set();
+// 			   		setTimeout()
+// 			   		stepperEnable.set(0);
+// 			   	}
+// 			   })
+// 			});
 
-		   }
-		});
-	   }
-	});
-   }
-});
-
-
+// 		   }
+// 		});
+// 	   }
+// 	});
+//    }
+// });
 
