9908af08287749418102396bfab9faa205a437b9
Diastowo <diastowo@gmail.com>
2020-05-29 01:11:15 +0700

    johny five

--- test.js
+++ test.js
@@ -1,41 +1,41 @@
-var Gpio = require('onoff').Gpio;
+// var Gpio = require('onoff').Gpio;
 
-var stepperEnable = new Gpio(13, 'out');
-var stepperDir = new Gpio(19, 'out');
-var stepperPulse = new Gpio(26, 'out');
+// var stepperEnable = new Gpio(13, 'out');
+// var stepperDir = new Gpio(19, 'out');
+// var stepperPulse = new Gpio(26, 'out');
 
-let stopBlinking = false;
+// let stopBlinking = false;
 
-stepperDir.write(1, err => { // Asynchronous write
-	console.log('stepperDir')
-	if (err) {
-		throw err;
-	}
-	stepperPulse.write(1, err => { // Asynchronous write
-		console.log('stepperPulse')
-		if (err) {
-			throw err;
-		}
-		for (var i=0; i<1600; i++) {
-			setTimeout(function(){
-				stepperEnable.write(1, err => {
-					console.log('stepperEnable1')
-					if (err) {
-						throw err;
-					}
-			    });
-			}, 500)
-			setTimeout(function(){
-				stepperEnable.write(0, err => {
-					console.log('stepperEnable0')
-					if (err) {
-						throw err;
-					}
-			    });
-			}, 500)
-		}
-	});
-});
+// stepperDir.write(1, err => { // Asynchronous write
+// 	console.log('stepperDir')
+// 	if (err) {
+// 		throw err;
+// 	}
+// 	stepperPulse.write(1, err => { // Asynchronous write
+// 		console.log('stepperPulse')
+// 		if (err) {
+// 			throw err;
+// 		}
+// 		for (var i=0; i<1600; i++) {
+// 			setTimeout(function(){
+// 				stepperEnable.write(1, err => {
+// 					console.log('stepperEnable1')
+// 					if (err) {
+// 						throw err;
+// 					}
+// 			    });
+// 			}, 5000)
+// 			setTimeout(function(){
+// 				stepperEnable.write(0, err => {
+// 					console.log('stepperEnable0')
+// 					if (err) {
+// 						throw err;
+// 					}
+// 			    });
+// 			}, 5000)
+// 		}
+// 	});
+// });
 
 
 // const blinkLed = _ => {
@@ -103,3 +103,42 @@ stepperDir.write(1, err => { // Asynchronous write
 //    }
 // });
 
+const {Board, Stepper} = require("johnny-five");
+const board = new Board();
+
+board.on("ready", () => {
+
+  /**
+   * In order to use the Stepper class, your board must be flashed with
+   * either of the following:
+   *
+   * - AdvancedFirmata https://github.com/soundanalogous/AdvancedFirmata
+   * - ConfigurableFirmata https://github.com/firmata/arduino/releases/tag/v2.6.2
+   *
+   */
+
+  const stepper = new Stepper({
+    type: Stepper.TYPE.DRIVER,
+    stepsPerRev: 200,
+    pins: {
+      step: 13,
+      dir: 19
+    }
+  });
+
+  // Set stepper to 180 RPM, counter-clockwise with acceleration and deceleration
+  stepper.rpm(180).ccw().accel(1600).decel(1600);
+  
+  // Make 10 full revolutions
+  stepper.step(2000, () => {
+
+    console.log("Done moving CCW");
+
+    // once first movement is done, make 10 revolutions clockwise at previously
+    //      defined speed, accel, and decel by passing an object into stepper.step
+    stepper.step({
+      steps: 2000,
+      direction: Stepper.DIRECTION.CW
+    }, () => console.log("Done moving CW"));
+  });
+});
