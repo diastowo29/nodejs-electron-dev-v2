df9381317a6eae8d3ba289018127ca0044e55ea9
Diastowo <diastowo@gmail.com>
2020-05-29 13:41:02 +0700

    strobe

--- test.js
+++ test.js
@@ -109,39 +109,30 @@ var board = new five.Board({
   io: new Raspi()
 });
 
-board.on("ready", () => {
+board.on("ready", function() {
+  var events = [];
+  var strobe = new five.Pin(13);
 
-  /**
-   * In order to use the Stepper class, your board must be flashed with
-   * either of the following:
-   *
-   * - AdvancedFirmata https://github.com/soundanalogous/AdvancedFirmata
-   * - ConfigurableFirmata https://github.com/firmata/arduino/releases/tag/v2.6.2
-   *
-   */
-
-  const stepper = new Stepper({
-    type: Stepper.TYPE.DRIVER,
-    stepsPerRev: 200,
-    pins: {
-      step: 26,
-      dir: 19
-    }
+  temporal.loop(500, function(loop) {
+    strobe[loop.called % 2 === 0 ? "high" : "low"]();
   });
 
-  // Set stepper to 180 RPM, counter-clockwise with acceleration and deceleration
-  stepper.rpm(180).ccw().accel(1600).decel(1600);
-  
-  // Make 10 full revolutions
-  stepper.step(2000, () => {
-
-    console.log("Done moving CCW");
-
-    // once first movement is done, make 10 revolutions clockwise at previously
-    //      defined speed, accel, and decel by passing an object into stepper.step
-    stepper.step({
-      steps: 2000,
-      direction: Stepper.DIRECTION.CW
-    }, () => console.log("Done moving CW"));
+  // Pin emits "high" and "low" events, whether it's
+  // input or output.
+  ["high", "low"].forEach(function(state) {
+    strobe.on(state, function() {
+      if (events.indexOf(state) === -1) {
+        console.log("Event emitted for:", state, "on", this.addr);
+        events.push(state);
+      }
+    });
   });
+
+  // var analog = new five.Pin("A0");
+
+  // // Query the analog pin for its current state.
+  // analog.query(function(state) {
+  //   console.log(state);
+  // });
+
 });
