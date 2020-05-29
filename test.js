183c70fefa877ee71b22831fb75973ddfc58a038
Diastowo <diastowo@gmail.com>
2020-05-29 13:55:32 +0700

    strobe

--- test.js
+++ test.js
@@ -112,28 +112,17 @@ var board = new five.Board({
 
 board.on("ready", function() {
   var events = [];
-  var strobe = new five.Pin(13);
+  // var strobe = new five.Pin(13);
 
-  temporal.loop(500, function(loop) {
-    strobe[loop.called % 2 === 0 ? "high" : "low"]();
+  const led = new Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  board.repl.inject({
+    led
   });
 
-  // Pin emits "high" and "low" events, whether it's
-  // input or output.
-  ["high", "low"].forEach(function(state) {
-    strobe.on(state, function() {
-      if (events.indexOf(state) === -1) {
-        console.log("Event emitted for:", state, "on", this.addr);
-        events.push(state);
-      }
-    });
-  });
-
-  // var analog = new five.Pin("A0");
-
-  // // Query the analog pin for its current state.
-  // analog.query(function(state) {
-  //   console.log(state);
-  // });
+  led.blink();
 
 });
