7a27ac0fdfcfa7106c2440701925668c572e8f4f
Diastowo <diastowo@gmail.com>
2020-05-29 14:16:32 +0700

    strobe

--- test.js
+++ test.js
@@ -66,77 +66,14 @@
 
 // setTimeout(_ => stopBlinking = true, 5000);
 
-// var gpio = require("gpio");
+var gpio = require("gpio");
 
-// var stepperEnable = gpio.export(13, {
-//    direction: gpio.DIRECTION.OUT,
-//    interval: 200,
-//    ready: function() {
-//    	console.log('stepperEnable ready')
-// 	var stepperDir = gpio.export(19, {
-// 	   direction: gpio.DIRECTION.OUT,
-// 	   interval: 200,
-// 	   ready: function() {
-// 	   	console.log('stepperDir ready')
-// 		var stepperPulse = gpio.export(26, {
-// 		   direction: gpio.DIRECTION.OUT,
-// 		   interval: 200,
-// 		   ready: function() {
-// 		   	console.log('stepperPulse ready')
-
-// 			stepperPulse.set(function() {
-// 			   console.log('stepperPulse: ' + stepperPulse.value);
-// 			   stepperDir.set(function() {
-// 			   	console.log('stepperDir: ' + stepperDir.value);
-// 			   	for (var i=0; i<1600; i++) {
-// 			   		stepperEnable.set();
-// 			   		setTimeout()
-// 			   		stepperEnable.set(0);
-// 			   	}
-// 			   })
-// 			});
-
-// 		   }
-// 		});
-// 	   }
-// 	});
-//    }
-// });
-var five = require("johnny-five");
-const {Board, Stepper, Led} = require("johnny-five");
-var temporal = require("temporal");
-var Raspi = require("raspi-io").RaspiIO;
-var board = new five.Board({
-  io: new Raspi()
-});
-
-board.on("ready", function() {
-  const led = new Led(13);
-  board.repl.inject({
-    led
-  });
-  led.on();
-
-  // temporal.loop(500, function(loop) {
-  //   strobe[loop.called % 2 === 0 ? "high" : "low"]();
-  // });
-
-  // // Pin emits "high" and "low" events, whether it's
-  // // input or output.
-  // ["high", "low"].forEach(function(state) {
-  //   strobe.on(state, function() {
-  //     if (events.indexOf(state) === -1) {
-  //       console.log("Event emitted for:", state, "on", this.addr);
-  //       events.push(state);
-  //     }
-  //   });
-  // });
-
-  // var analog = new five.Pin("A0");
-
-  // // Query the analog pin for its current state.
-  // analog.query(function(state) {
-  //   console.log(state);
-  // });
-
+var stepperEnable = gpio.export(13, {
+   direction: gpio.DIRECTION.OUT,
+   interval: 200,
+   ready: function() {
+    console.log('stepperEnable ready')
+    setInterval(stepperEnable.set(), 500);
+    setTimeout(stepperEnable.set(0), 1500);
+   }
 });
