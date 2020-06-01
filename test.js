33c39abaa1684c68cb9189047d67863fffff8efa
Diastowo <diastowo@gmail.com>
2020-06-02 00:56:24 +0700

    still fixing

--- test.js
+++ test.js
@@ -71,21 +71,21 @@ const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
 console.log('watch')
 trigger.digitalWrite(0); // Make sure trigger is low
 
-const watchHCSR04 = () => {
-  let startTick;
+// const watchHCSR04 = () => {
+// };
+let startTick;
 
-  echo.on('alert', (level, tick) => {
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(diff / 2 / MICROSECDONDS_PER_CM);
-    }
-  });
-};
+echo.on('alert', (level, tick) => {
+  if (level == 1) {
+    startTick = tick;
+  } else {
+    const endTick = tick;
+    const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+    console.log(diff / 2 / MICROSECDONDS_PER_CM);
+  }
+});
 
-watchHCSR04();
+// watchHCSR04();
 
 // Trigger a distance measurement once per second
 setInterval(() => {
