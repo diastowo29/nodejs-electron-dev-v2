b1ced0124360fc6e543ba6129ac772c0742604d3
Diastowo <diastowo@gmail.com>
2020-06-02 02:36:52 +0700

    still fixing

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ var Gpio = require('onoff').Gpio;
 // var ledPin = new Gpio(3, 'out');
 
 var trigger = new Gpio(23, 'out');
-var echo = new Gpio(24, 'in', 'falling');
+var echo = new Gpio(24, 'in', 'rising');
 
 var timeTick;
 var timeTock;
