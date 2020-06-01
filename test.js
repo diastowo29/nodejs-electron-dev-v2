9f0e9db5933cd586825c5cf3d9ee733810591d03
Diastowo <diastowo@gmail.com>
2020-06-01 18:43:30 +0700

    testing

--- test.js
+++ test.js
@@ -9,49 +9,49 @@ pinDir.writeSync(1)
 
 stepCounter = 0;
 
-// var blinkInterval = setInterval(blinkLED, 50);
+var blinkInterval = setInterval(blinkLED, 10);
 
 var subsidi = 3;
 
-for (var i=0; i<1600; i++) {
-  console.log('pinPulse: %s', pinPulse.readSync());
-  pinPulse.writeSync(1);
-  wait(10)
-  console.log('pinPulse: %s', pinPulse.readSync());
-  pinPulse.writeSync(0)
-  wait(10)
+// for (var i=0; i<1600; i++) {
+//   console.log('pinPulse: %s', pinPulse.readSync());
+//   pinPulse.writeSync(1);
+//   wait(10)
+//   console.log('pinPulse: %s', pinPulse.readSync());
+//   pinPulse.writeSync(0)
+//   wait(10)
+// }
+
+function blinkLED() {
+  stepCounter++;
+  console.log(stepCounter)
+  console.log('pinPulse: %s', pinPulse.readSync())
+  if (pinPulse.readSync() === 0) {
+    pinPulse.writeSync(1);
+    ledPin.writeSync(1);
+  } else {
+    pinPulse.writeSync(0);
+    ledPin.writeSync(0);
+  }
+  if (stepCounter == 3200) {
+    clearInterval(blinkInterval);
+  }
 }
 
-// function blinkLED() {
-//   stepCounter++;
-//   console.log(stepCounter)
-//   console.log('pinPulse: %s', pinPulse.readSync())
-//   if (pinPulse.readSync() === 0) {
-//     pinPulse.writeSync(1);
-//     ledPin.writeSync(1);
-//   } else {
-//     pinPulse.writeSync(0);
-//     ledPin.writeSync(0);
-//   }
-//   if (stepCounter == 3200) {
-//     clearInterval(blinkInterval);
-//   }
-// }
+function endBlink() {
+  console.log('end process')
+  clearInterval(blinkInterval);
+  pinPulse.writeSync(0);
+  pinDir.writeSync(0);
+  pinEnable.writeSync(0);
+  ledPin.writeSync(0);
+  pinPulse.unexport();
+  pinDir.unexport();
+  pinEnable.unexport();
+  ledPin.unexport();
+}
 
-// function endBlink() {
-//   console.log('end process')
-//   clearInterval(blinkInterval);
-//   pinPulse.writeSync(0);
-//   pinDir.writeSync(0);
-//   pinEnable.writeSync(0);
-//   ledPin.writeSync(0);
-//   pinPulse.unexport();
-//   pinDir.unexport();
-//   pinEnable.unexport();
-//   ledPin.unexport();
-// }
-
-// setTimeout(endBlink, 50000);
+setTimeout(endBlink, 50000);
 
 // process.on('SIGINT', endBlink);
 
