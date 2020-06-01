1a21b0fb4a72faebce9258a33b0e0cb9abc7013f
Diastowo <diastowo@gmail.com>
2020-06-01 18:44:59 +0700

    testing

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var Gpio = require('onoff').Gpio;
 var pinEnable = new Gpio(13, 'out');
 var pinDir = new Gpio(19, 'out');
-var pinPulse = new Gpio(26, 'out');
+var pinPulse = new Gpio(21, 'out');
 // var ledPin = new Gpio(3, 'out');
 
 pinEnable.writeSync(0)
@@ -13,45 +13,45 @@ var blinkInterval = setInterval(blinkLED, 10);
 
 var subsidi = 3;
 
-// for (var i=0; i<1600; i++) {
-//   console.log('pinPulse: %s', pinPulse.readSync());
-//   pinPulse.writeSync(1);
-//   wait(10)
-//   console.log('pinPulse: %s', pinPulse.readSync());
-//   pinPulse.writeSync(0)
-//   wait(10)
+for (var i=0; i<1600; i++) {
+  console.log('pinPulse: %s', pinPulse.readSync());
+  pinPulse.writeSync(1);
+  wait(10)
+  console.log('pinPulse: %s', pinPulse.readSync());
+  pinPulse.writeSync(0)
+  wait(10)
+}
+
+// function blinkLED() {
+//   stepCounter++;
+//   console.log(stepCounter)
+//   console.log('pinPulse: %s', pinPulse.readSync())
+//   if (pinPulse.readSync() === 0) {
+//     pinPulse.writeSync(1);
+//     ledPin.writeSync(1);
+//   } else {
+//     pinPulse.writeSync(0);
+//     ledPin.writeSync(0);
+//   }
+//   if (stepCounter == 3200) {
+//     clearInterval(blinkInterval);
+//   }
 // }
 
-function blinkLED() {
-  stepCounter++;
-  console.log(stepCounter)
-  console.log('pinPulse: %s', pinPulse.readSync())
-  if (pinPulse.readSync() === 0) {
-    pinPulse.writeSync(1);
-    ledPin.writeSync(1);
-  } else {
-    pinPulse.writeSync(0);
-    ledPin.writeSync(0);
-  }
-  if (stepCounter == 3200) {
-    clearInterval(blinkInterval);
-  }
-}
+// function endBlink() {
+//   console.log('end process')
+//   clearInterval(blinkInterval);
+//   pinPulse.writeSync(0);
+//   pinDir.writeSync(0);
+//   pinEnable.writeSync(0);
+//   ledPin.writeSync(0);
+//   pinPulse.unexport();
+//   pinDir.unexport();
+//   pinEnable.unexport();
+//   ledPin.unexport();
+// }
 
-function endBlink() {
-  console.log('end process')
-  clearInterval(blinkInterval);
-  pinPulse.writeSync(0);
-  pinDir.writeSync(0);
-  pinEnable.writeSync(0);
-  ledPin.writeSync(0);
-  pinPulse.unexport();
-  pinDir.unexport();
-  pinEnable.unexport();
-  ledPin.unexport();
-}
-
-setTimeout(endBlink, 50000);
+// setTimeout(endBlink, 50000);
 
 // process.on('SIGINT', endBlink);
 
