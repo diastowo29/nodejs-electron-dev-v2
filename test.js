fdc6a3f7ddc296641c0431ae2cba958cac6d7e65
Diastowo <diastowo@gmail.com>
2020-06-02 10:54:41 +0700

    still testing sonic

--- test.js
+++ test.js
@@ -89,37 +89,37 @@
 
 
 
-const Gpio = require('pigpio').Gpio;
+// const Gpio = require('pigpio').Gpio;
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6/34321;
+// // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+// const MICROSECDONDS_PER_CM = 1e6/34321;
 
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+// const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
+// const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
 
-console.log('watch')
-trigger.digitalWrite(0); // Make sure trigger is low
+// console.log('watch')
+// trigger.digitalWrite(0); // Make sure trigger is low
 
-// const watchHCSR04 = () => {
-// };
-let startTick;
+// // const watchHCSR04 = () => {
+// // };
+// let startTick;
 
-echo.on('alert', (level, tick) => {
-  if (level == 1) {
-    startTick = tick;
-  } else {
-    const endTick = tick;
-    const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-    console.log(diff / 2 / MICROSECDONDS_PER_CM);
-  }
-});
+// echo.on('alert', (level, tick) => {
+//   if (level == 1) {
+//     startTick = tick;
+//   } else {
+//     const endTick = tick;
+//     const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+//     console.log(diff / 2 / MICROSECDONDS_PER_CM);
+//   }
+// });
 
-// watchHCSR04();
+// // watchHCSR04();
 
-// Trigger a distance measurement once per second
-setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000);
+// // Trigger a distance measurement once per second
+// setInterval(() => {
+//   trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+// }, 1000);
 
 
 
