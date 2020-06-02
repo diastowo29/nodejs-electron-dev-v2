7d32f05654a6f5ad0d6457018dc405ae768f4c8d
Diastowo <diastowo@gmail.com>
2020-06-02 09:15:23 +0700

    still testing sonic

--- test.js
+++ test.js
@@ -1,39 +1,37 @@
-// var Gpio = require('onoff').Gpio;
-// // var pinEnable = new Gpio(13, 'out');
-// // var pinDir = new Gpio(19, 'out');
-// // var pinPulse = new Gpio(21, 'out');
-// // var ledPin = new Gpio(3, 'out');
+var Gpio = require('onoff').Gpio;
+// var pinEnable = new Gpio(13, 'out');
+// var pinDir = new Gpio(19, 'out');
+// var pinPulse = new Gpio(21, 'out');
+// var ledPin = new Gpio(3, 'out');
 
-// var trigger = new Gpio(23, 'out');
-// var echo = new Gpio(24, 'in', 'rising');
+var trigger = new Gpio(23, 'out');
+var echo = new Gpio(24, 'in', 'rising');
 
-// var timeTick;
-// var timeTock;
+var timeTick;
+var timeTock;
 
-// trigger.writeSync(0)
+setInterval(function() {
+  console.log('triggering')
+  trigger.writeSync(0)
+  wait(2)
+  timeTick = new Date().getTime();
+  trigger.writeSync(1)
+  wait(10)
+  trigger.writeSync(0)
+}, 1000);
 
-// echo.watch((err, value) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log(value);
-//   if (value == 1) {
-//     timeTock = new Date().getTime()
-//     console.log(timeTick)
-//     console.log(timeTock)
-//     console.log(timeTick - timeTock)
-//   }
-// });
-
-// setInterval(function() {
-//   console.log('setting triger')
-//   if (trigger.readSync() == 1) {
-//     trigger.writeSync(0)
-//   } else {
-//     trigger.writeSync(1)
-//     timeTick = new Date().getTime();
-//   }
-// }, 1000)
+echo.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+  console.log(value);
+  if (value == 1) {
+    timeTock = new Date().getTime()
+    console.log(timeTick)
+    console.log(timeTock)
+    console.log(timeTick - timeTock)
+  }
+});
 
 // pinEnable.writeSync(0)
 // pinDir.writeSync(1)
@@ -91,37 +89,37 @@
 
 
 
-const Gpio = require('pigpio').Gpio;
+// const Gpio = require('pigpio').Gpio;
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6/34321;
+// // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+// const MICROSECDONDS_PER_CM = 1e6/34321;
 
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
+// const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
+// const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
 
-console.log('watch')
-trigger.digitalWrite(0); // Make sure trigger is low
+// console.log('watch')
+// trigger.digitalWrite(0); // Make sure trigger is low
 
-// const watchHCSR04 = () => {
-// };
-let startTick;
+// // const watchHCSR04 = () => {
+// // };
+// let startTick;
 
-echo.on('alert', (level, tick) => {
-  if (level == 1) {
-    startTick = tick;
-  } else {
-    const endTick = tick;
-    const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-    console.log(diff / 2 / MICROSECDONDS_PER_CM);
-  }
-});
+// echo.on('alert', (level, tick) => {
+//   if (level == 1) {
+//     startTick = tick;
+//   } else {
+//     const endTick = tick;
+//     const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+//     console.log(diff / 2 / MICROSECDONDS_PER_CM);
+//   }
+// });
 
-// watchHCSR04();
+// // watchHCSR04();
 
-// Trigger a distance measurement once per second
-setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000);
+// // Trigger a distance measurement once per second
+// setInterval(() => {
+//   trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+// }, 1000);
 
 
 
@@ -163,5 +161,5 @@ function wait(ms){
             // var newKuota = intKuota - jatahSubs;
             // console.log(newKuota)
 
-var timeNow = new Date().getTime()
-console.log(timeNow);
+// var timeNow = new Date().getTime()
+// console.log(timeNow);
