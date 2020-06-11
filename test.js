b21004e68c4cbd897e4e27b20b64b8d5a0d5ebe0
Diastowo <diastowo@gmail.com>
2020-06-11 14:48:29 +0700

    nanotimer

--- test.js
+++ test.js
@@ -302,7 +302,7 @@ function main() {
   pinEnable.writeSync(0)
   pinDir.writeSync(1)
 
-  timer.setInterval(stepperGo, '', '10u');
+  timer.setInterval(stepperGo, '', '4u');
   timer.setTimeout(stepperOff, [timer], '10s');
 }
 
